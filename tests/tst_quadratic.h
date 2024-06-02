c6c2e976aa0b5185a87d6d0549fe7671d9593512
vikimova <wikimova@gmail.com>
2024-06-02 18:57:09 +0200

    fix(tests)

--- tests/tst_quadratic.h
+++ tests/tst_quadratic.h
@@ -5,13 +5,22 @@ extern "C" {
 }
 
 TEST(TestNoErrorQudraticRoots, NormalRoots) {
-    ASSERT_EQ(calc_sqrt_roots(1, 2, -3), {1, -3, 0});
+    Roots roots = {1, -3};
+    Roots *r = &roots;
+    Result res = {r, 0};
+    ASSERT_EQ(calc_sqrt_roots(1, 2, -3), res);
 }
 
 TEST(TestNoErrorQudraticRoots, EqualRoots) {
-	ASSERT_EQ(calc_sqrt_roots(1, -2, 1), {0, 0, 0});
+    Roots roots = {0, 0};
+    Roots *r = &roots;
+    Result res = {r, 0};
+    ASSERT_EQ(calc_sqrt_roots(1, -2, 1), res);
 }
 
 TEST(TestQuadraticWithErrorRoots, PositiveOk) {
-	ASSERT_EQ(calc_sqrt_roots(-1, -7, -19), {-1, -1, 1});
+    Roots roots = {-1, -1};
+    Roots *r = &roots;
+    Result res = {r, 1};
+    ASSERT_EQ(calc_sqrt_roots(-1, -7, -19), res);
 }
